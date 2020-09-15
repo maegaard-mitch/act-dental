987ff18636d43f02b0f25b09dfbb1a1d935d3153
maegaard-mitch <68915811+maegaard-mitch@users.noreply.github.com>
2020-09-15 08:03:27 -0500

    initial commit of database script

---
+++ database.py
@@ -0,0 +1,53 @@
+import psycopg2 as pg
+import psycopg2.extras as extras
+from config import config
+
+def connect():
+    con = None
+    params = config()
+    
+    try:
+        con = pg.connect(**params)
+    except (Exception, pg.DatabaseError) as error:
+        print(error)
+    
+    return con
+
+def execute_query(conn, query):
+    
+    ret = 0 # Return value
+    cursor = conn.cursor()
+    try:
+        cursor.execute(query)
+        conn.commit()
+    except (Exception, pg.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    
+    # If this was a select query, return the result
+    if 'select' in query.lower():
+        ret = cursor.fetchall()
+    cursor.close()
+    return ret
+
+def execute_values(conn, df, table):
+    
+    # Create a list of tupples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    # SQL quert to execute
+    query  = "INSERT INTO %s(%s) VALUES %%s" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_values(cursor, query, tuples)
+        conn.commit()
+    except (Exception, pg.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_values() done")
+    cursor.close()
